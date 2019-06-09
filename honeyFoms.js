b9bb0c880166102cf630d18716f9dbe1ffe22092
tahmasebi <sara66@protonmail.com>
2019-06-09 16:40:09 +0430

    adding interaction to dots

---
+++ honeyFoms.js
@@ -0,0 +1,21 @@
+
+var formOne = {
+    id: "#firstDot",
+    form: "Liquid Honey",
+    subject: "The One We All<br>Know And Love",
+    number: "01",
+};
+
+var formTwo = {
+    id: "#secondDot",
+    form: "Comb Honey",
+    subject: "Honey Inside Of<br>The Honeycomb",
+    number: "02",
+};
+
+var formThree = {
+    id: "#thirdDot",
+    form: "Crystallized Honey",
+    subject: "Glucose Content<br>Has Crystallized",
+    number: "03",
+};
--- main.js
+++ main.js
@@ -2,3 +2,20 @@
 function toggleListBox(){
     $("#listBox").toggle();
 }
+
+function changeFormOfHoney(dot, honeyId) {
+    
+    /* change the current dot */
+    $("#dots i.current").removeClass("current");
+    $(dot.id).addClass("current");
+
+    /* change the subject content and counter */
+    $("#subjectContent h4").html(dot.form);
+    $("#subject").html(dot.subject);
+    $("#counter").html(dot.number);
+
+    /* change the selected bottom bar */
+    $("#bottomNav li.selected").removeClass("selected");
+    $(honeyId).addClass("selected");
+}
+
